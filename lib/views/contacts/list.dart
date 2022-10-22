a3ad71597b9c2f1994f9e2839fd099c50ea0a94d
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2022-10-22 20:51:15 -0300

    Adicionado a lista de contatos

---
+++ lib/views/contacts/list.dart
@@ -0,0 +1,32 @@
+import 'package:bytebank2/views/contacts/form.dart';
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  const ContactsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Contacts',
+        ),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              title: Text('Alex'),
+              subtitle: const Text('1000'),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => ContactForm())),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
