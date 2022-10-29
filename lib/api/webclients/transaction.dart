9d95e69dc92c2ae44fce876ca55a59755ea9264c
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2022-10-29 03:54:23 -0300

    Refatorando código utilizando o fromJson e o toJson da doc

--- lib/api/webclients/transaction.dart
+++ lib/api/webclients/transaction.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 
-import '../../models/contact.dart';
 import '../../views/transactions/list.dart';
 import '../webclient.dart';
 
@@ -14,50 +13,23 @@ class TransactionWebClient {
     final List<dynamic> decodedJson = jsonDecode(response.body);
     final List<Transaction> transactions = [];
     for (Map<String, dynamic> transactionJson in decodedJson) {
-      final Map<String, dynamic> contactJson = transactionJson['contact'];
-      final Transaction transaction = Transaction(
-        transactionJson['value'],
-        Contact(
-          0,
-          contactJson['name'],
-          contactJson['accountNumber'],
-        ),
-      );
-      transactions.add(transaction);
+      transactions.add(Transaction.fromJson(transactionJson));
     }
     return transactions;
   }
 
   Future<Transaction> save(Transaction transaction) async {
-    final Contact contact = transaction.contact;
-    final Map<String, dynamic> transactionMap = {
-      'value': transaction.value,
-      'contact': {
-        'name': contact.name,
-        'accountNumber': contact.accountNumber,
-      }
-    };
+    final String transactionJson = jsonEncode(transaction.toJson());
 
-    final String transactionJson = jsonEncode(transactionMap);
-    final Response response = await client.post(
-      Uri.parse(baseUrl),
-      headers: {'Content-type': 'application/json', 'password': '1000'},
-      body: transactionJson,
-    );
+    final Response response = await client.post(Uri.parse(baseUrl),
+        headers: {'Content-type': 'application/json', 'password': '1000'},
+        body: transactionJson);
 
     return _toTransaction(response);
   }
 
   Transaction _toTransaction(Response response) {
     Map<String, dynamic> json = jsonDecode(response.body);
-    final Map<String, dynamic> contactJson = json['contact'];
-    return Transaction(
-      json['value'],
-      Contact(
-        0,
-        contactJson['name'],
-        contactJson['accountNumber'],
-      ),
-    );
+    return Transaction.fromJson(json);
   }
 }
